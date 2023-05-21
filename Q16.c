6f659aaee257c1f161ea311d2f61d4b2085e3da4
Arindamsharma12 <117537886+Arindamsharma12@users.noreply.github.com>
2023-05-21 15:05:40 +0530

    Q16.c

---
+++ Q16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int n,i;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    for(i = 2; i < n; i++){
+        if(n % i == 0){
+            printf("Non-prime");
+            break;
+        }
+    }
+    if(i == n){
+        printf("Prime");
+    }
+    return 0;
+}
